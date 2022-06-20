e6c2e6a330f8e63ed5c6a7c4b74b0f01bed9150b
Ardian Pramudya Alphita <ardianpramudya81@gmail.com>
2022-06-20 11:32:25 +0700

    Initial Commit

---
+++ Sleep/SleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepApp.swift
+//  Sleep
+//
+//  Created by Ardian Pramudya Alphita on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

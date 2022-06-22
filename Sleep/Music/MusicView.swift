a7389f89bbc91782bf659392afef945338cbfb54
Avian Lukman Setya Budi <avianlukmans@gmail.com>
2022-06-22 10:22:24 +0700

    Tab Bar

---
+++ Sleep/Music/MusicView.swift
@@ -0,0 +1,20 @@
+//
+//  MusicView.swift
+//  Sleep
+//
+//  Created by Avian Lukman Setya Budi on 22/06/22.
+//
+
+import SwiftUI
+
+struct MusicView: View {
+    var body: some View {
+        Text("Ini Musik View")
+    }
+}
+
+struct MusicView_Previews: PreviewProvider {
+    static var previews: some View {
+        MusicView()
+    }
+}
